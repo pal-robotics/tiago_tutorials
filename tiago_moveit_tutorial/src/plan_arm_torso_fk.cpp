552c71318646dcd21e71d8852df562047342fe48
Jordi Pages <jordi.pages@pal-robotics.com>
2016-12-23 09:38:00 +0100

    first version of moveit tutorial

---
+++ tiago_moveit_tutorial/src/plan_arm_torso_fk.cpp
@@ -0,0 +1,116 @@
+/*
+ * Software License Agreement (Modified BSD License)
+ *
+ *  Copyright (c) 2016, PAL Robotics, S.L.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of PAL Robotics, S.L. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/** \author Jordi Pages. */
+
+// ROS headers
+#include <ros/ros.h>
+
+// MoveIt! headers
+#include <moveit/move_group_interface/move_group.h>
+
+// Std C++ headers
+#include <string>
+#include <vector>
+#include <map>
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "plan_arm_torso_fk");
+
+  if ( argc < 9 )
+  {
+    ROS_INFO(" ");
+    ROS_INFO("\tUsage:");
+    ROS_INFO(" ");
+    ROS_INFO("\trosrun tiago_moveit_tutorial plan_arm_torso_fk torso_lift arm_1 arm_2 arm_3 arm_4 arm_5 arm_6 arm_7 arm_8");
+    ROS_INFO(" ");
+    ROS_INFO("\twhere the list of arguments are the target values for the given joints");
+    ROS_INFO(" ");
+    return EXIT_FAILURE;
+  }
+
+  std::map<std::string, double> target_position;
+
+  target_position["torso_lift_joint"] = atof(argv[1]);
+  target_position["arm_1_joint"] = atof(argv[2]);
+  target_position["arm_2_joint"] = atof(argv[3]);
+  target_position["arm_3_joint"] = atof(argv[4]);
+  target_position["arm_4_joint"] = atof(argv[5]);
+  target_position["arm_5_joint"] = atof(argv[6]);
+  target_position["arm_6_joint"] = atof(argv[7]);
+  target_position["arm_7_joint"] = atof(argv[8]);
+
+  ros::NodeHandle nh;
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+
+  std::vector<std::string> torso_arm_joint_names;
+  //select group of joints
+  moveit::planning_interface::MoveGroup group_arm_torso("arm_torso");
+  //choose your preferred planner
+  group_arm_torso.setPlannerId("SBLkConfigDefault");
+
+  torso_arm_joint_names = group_arm_torso.getJoints();
+
+  group_arm_torso.setStartStateToCurrentState();
+  group_arm_torso.setMaxVelocityScalingFactor(1.0);
+
+  for (unsigned int i = 0; i < torso_arm_joint_names.size(); ++i)
+    if ( target_position.count(torso_arm_joint_names[i]) > 0 )
+    {
+      ROS_INFO_STREAM("\t" << torso_arm_joint_names[i] << " goal position: " << target_position[torso_arm_joint_names[i]]);
+      group_arm_torso.setJointValueTarget(torso_arm_joint_names[i], target_position[torso_arm_joint_names[i]]);
+    }
+
+  moveit::planning_interface::MoveGroup::Plan my_plan;
+  group_arm_torso.setPlanningTime(5.0);
+  bool success = group_arm_torso.plan(my_plan);
+
+  if ( !success )
+    throw std::runtime_error("No plan found");
+
+  ROS_INFO_STREAM("Plan found in " << my_plan.planning_time_ << " seconds");
+
+  // Execute the plan
+  ros::Time start = ros::Time::now();
+
+  group_arm_torso.move();
+
+  ROS_INFO_STREAM("Motion duration: " << (ros::Time::now() - start).toSec());
+
+  spinner.stop();
+
+  return EXIT_SUCCESS;
+}
